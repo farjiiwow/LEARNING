1ac0df74087dcf47a9a7e961b9c41321fdca672c
farjiiwow <omdewang31122003@gmail.com>
2024-10-07 18:25:45 +0530

    continue keyword

--- C++/YT/loop.cpp
+++ C++/YT/loop.cpp
@@ -8,7 +8,8 @@ int main(void) {
 
     while(i < 7) {
         if(i == 3) {
-            break;
+            i++;
+            continue;
         }
         cout << "Current number is: " << arr[i] << endl;
         i = i + 1;
