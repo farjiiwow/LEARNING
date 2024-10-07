bfd2255b63ef4619a61908f5163de4fbf33c5674
farjiiwow <omdewang31122003@gmail.com>
2024-10-07 18:30:00 +0530

    understand do while

--- C++/YT/loop.cpp
+++ C++/YT/loop.cpp
@@ -4,16 +4,14 @@ int main(void) {
     
     int arr[] = {4, 5, 6, 7, 8, 9, 0};
     
-    int i = 0;
+    int i = 8;
 
-    while(i < 7) {
-        if(i == 3) {
-            i++;
-            continue;
-        }
+    do {
         cout << "Current number is: " << arr[i] << endl;
         i = i + 1;
-    }
+    } while(i < 7);
+
+
 
     return 0;
 }
