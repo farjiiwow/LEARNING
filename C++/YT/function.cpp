1db18f6099ccd521b35d727f44e018432635c254
farjiiwow <omdewang31122003@gmail.com>
2024-10-15 21:02:04 +0530

    fun with func

--- C++/YT/function.cpp
+++ C++/YT/function.cpp
@@ -9,10 +9,15 @@ int sayTwo(void) {
     return 3;
 }
 
+void sayThree(void) {
+    puts("4");
+}
+
 int main(void) {
     
     sayHello();
-    printf("%d\n", sayTwo());
+    printf("%d\n", sayTwo() + 2);
+    sayThree();
     
     return 0;
 }
