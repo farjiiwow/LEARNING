af81cd4338cb8f97aa43677f782ec1414f768ab7
farjiiwow <omdewang31122003@gmail.com>
2024-10-15 20:38:09 +0530

    unknown exception handling

--- C++/YT/trycatch.cpp
+++ C++/YT/trycatch.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(void) {
-    float call_api = 2;
+    // float call_api = 2;
+    char call_api = 'f';
     try {
         cout << "trying to use API value\n";
         cout << "did some testing with api value\n";
@@ -11,6 +12,8 @@ int main(void) {
         cout << "integer exception handled\n";
     } catch (float f) {
         cout << "float exception handled\n";
+    } catch (...) {
+        cout << "something wen't wrong\n";
     }
 
     cout << "keep on moving with rest of code\n";
