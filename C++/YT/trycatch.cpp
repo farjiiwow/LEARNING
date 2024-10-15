c0067355da386f572a051b5d51c0b5c0dbe1a4d9
farjiiwow <omdewang31122003@gmail.com>
2024-10-15 20:28:12 +0530

    exception handling

---
+++ C++/YT/trycatch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int call_api = 2;
+    try {
+        cout << "trying to use API value\n";
+        cout << "did some testing with api value\n";
+        throw call_api;
+        cout << "NO code execute after return and throw\n";
+    } catch (int x) {
+        cout << "integer exception handled\n";
+    }
+
+    cout << "keep on moving with rest of code\n";
+    
+
+    return 0;
+}
