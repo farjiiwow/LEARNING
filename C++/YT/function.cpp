ff4b16707b90905e534fd73b3ad40060eadc5169
farjiiwow <omdewang31122003@gmail.com>
2024-10-15 20:55:35 +0530

    Understand function

---
+++ C++/YT/function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void sayHello(void) {
+    puts("hello there!");
+}
+
+void sayTwo(void) {
+    puts("2");
+}
+
+int main(void) {
+    
+    sayHello();
+    sayTwo();
+    
+    return 0;
+}
+
