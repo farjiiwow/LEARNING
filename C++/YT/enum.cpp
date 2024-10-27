4f2103e5aea8d19984347c1ce05c59929781feb1
farjiiwow <omdewang31122003@gmail.com>
2024-10-27 14:25:16 +0530

    bit reference

--- C++/YT/enum.cpp
+++ C++/YT/enum.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 // #define PI 3.14
 
-enum MsOffice {
-    BOLD = 5,
-    ITALICS,
-    UNDERLINE,
+//bit reference
+enum MsOffice: uint8_t {
+    BOLD = 0,
+    ITALICS = 2,
+    UNDERLINE = 4,
     COSSED
 };
 
