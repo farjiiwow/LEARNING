acdd3a4d5fc9e0ffebee8af64fe9907c79b5a747
farjiiwow <omdewang31122003@gmail.com>
2024-11-09 17:39:57 +0530

    add result variable

--- freeCodeCamp/javascript/pyramid_generator.js
+++ freeCodeCamp/javascript/pyramid_generator.js
@@ -3,13 +3,21 @@ const count = 8;
 let rows = [];
 
 function padRow(rowNumber, totalRowCount) {
-  console.log(
+  return (
     " ".repeat(totalRowCount - rowNumber) +
-      character.repeat(2 * rowNumber - 1) +
-      " ".repeat(totalRowCount - rowNumber)
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(totalRowCount - rowNumber)
   );
 }
 
 for (let i = 1; i < count; i++) {
   rows.push(padRow(i, count));
 }
+
+let result = "";
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
