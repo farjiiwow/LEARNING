3f225ef9bc531cc609c2a486cc95ba92d62eb578
farjiiwow <omdewang31122003@gmail.com>
2024-11-12 18:42:08 +0530

    call by value

---
+++ C++/YT/callByValue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void lifeup(int life) {
+    ++life;
+}
+
+int main(void) {
+    int life = 3;
+    lifeup(life);
+    cout << life << endl;
+    return 0;
+}
