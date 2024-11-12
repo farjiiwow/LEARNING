2c64ad54b7e2bf7c54ad149658fc9c62572c82da
farjiiwow <omdewang31122003@gmail.com>
2024-11-12 19:00:35 +0530

    testing

--- C++/YT/callByReference.cpp
+++ C++/YT/callByReference.cpp
@@ -5,9 +5,23 @@ void lifeup(int &life) {
     ++(life);
 }
 
+int addme(int a, int b) {
+    return a + b;
+}
+
+float addme(float a, float b) {
+    return a + b;
+}
+
 int main(void) {
     int life = 3;
     lifeup(life);
     cout << life << endl;
+
+    int v1 = 3;
+    int v2 = 4;
+    float v3 = 5.5;
+    float v4 = 6.3;
+    cout << addme(v3, v4) << endl;
     return 0;
 }
