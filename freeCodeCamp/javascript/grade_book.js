93a5310d8bec5415129ceba816d306c5a121d895
farjiiwow <omdewang31122003@gmail.com>
2024-11-20 15:07:03 +0530

    display score

--- freeCodeCamp/javascript/grade_book.js
+++ freeCodeCamp/javascript/grade_book.js
@@ -31,6 +31,23 @@ function hasPassingGrade(score) {
   }
 }
 
-console.log(hasPassingGrade(100));
-console.log(hasPassingGrade(53));
-console.log(hasPassingGrade(87));
+function studentMsg(totalScores, studentScore) {
+  if (getGrade(studentScore) === "F") {
+    return (
+      "Class average: " +
+      getAverage(totalScores) +
+      ". Your grade: " +
+      getGrade(studentScore) +
+      ". You failed the course."
+    );
+  } else {
+    return (
+      "Class average: " +
+      getAverage(totalScores) +
+      ". Your grade: " +
+      getGrade(studentScore) +
+      ". You passed the course."
+    );
+  }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
