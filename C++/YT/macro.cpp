b2920b9b720c13efd5f1ad2022095e445a563632
farjiiwow <omdewang31122003@gmail.com>
2024-11-20 20:53:22 +0530

    understand macro

---
+++ C++/YT/macro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define END return 0
+#define ENDMESSAGE cout << "programs end here\n"
+#define farjii int32_t
+
+using namespace std;
+
+int main(void) {
+    
+    farjii a = 5;
+    cout << a << endl;
+
+    ENDMESSAGE;
+    END;
+}
