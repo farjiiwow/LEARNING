65f42cdedac7aef2d8dc5d110857d0c7b4dea0cb
farjiiwow <omdewang31122003@gmail.com>
2025-01-27 00:03:57 +0530

    get to know console

---
+++ webDev/JSFOUNDATION/part1/printing.js
@@ -0,0 +1,9 @@
+console.log("hello");
+console.log("chai");
+process.stdout.write("chai");
+process.stdout.write("chai");
+
+console.table("test");
+
+console.table({ city: "Jaipur" });
+console.warn({ city: "Jaipur" });
