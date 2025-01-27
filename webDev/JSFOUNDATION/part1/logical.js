d2f4976be6a781f40e8fd33302a2d6889cc53767
farjiiwow <omdewang31122003@gmail.com>
2025-01-27 18:38:24 +0530

    logical

---
+++ webDev/JSFOUNDATION/part1/logical.js
@@ -0,0 +1,16 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedin = true;
+let isPaid = false;
+
+console.log(isLoggedin && isPaid);
+
+isPaid = true;
+console.log(isLoggedin && isPaid);
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
