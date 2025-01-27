624c6d817eb653459205d21869934930c266bdb9
farjiiwow <omdewang31122003@gmail.com>
2025-01-27 18:33:30 +0530

    operator

---
+++ webDev/JSFOUNDATION/part1/operations.js
@@ -0,0 +1,39 @@
+let score = 102;
+let bonus = 25;
+let totalScore = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let mul = 3 * 5;
+let divi = 8 / 2;
+let reminder = 9 % 2;
+
+let expo = 2 ** 3;
+
+let myscore = 110;
+myscore++;
+
+let credits = 56;
+credits--;
+
+console.log(totalScore);
+console.log(addition);
+console.log(subtract);
+console.log(mul);
+console.log(divi);
+console.log(reminder);
+console.log(expo);
+console.log(myscore);
+console.log(credits);
+
+// comparision operation
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2); //true
+console.log(num1 != num3); //true
+console.log(num1 == num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
