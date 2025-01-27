559bf6b6301876ed998af8d08f2ab436c33bb138
farjiiwow <omdewang31122003@gmail.com>
2025-01-27 16:51:30 +0530

    basic of datatype

---
+++ webDev/JSFOUNDATION/part1/datatype.js
@@ -0,0 +1,33 @@
+console.log("hello");
+// console.log("chai");
+/*
+
+string - set of characters
+number 
+boolean
+bigint
+
+Undefined - no definition
+null - empty (example - temprature, null temprature that means we don't have data)
+0 (0 temprature that means we are talking about 0deg)
+
+object
+
+symbol
+
+*/
+
+// var score = 102
+
+let score = 102;
+let name = "farjii";
+let isLoggedin = false;
+
+//object
+
+let teaTypes = ["Lemon tea", "orange tea", "oolong tea"];
+let user = { firstname: "farjii", lastname: "wow" };
+
+let getScore = score;
+
+console.log(getScore);
