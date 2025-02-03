f8c8aed491e742ad47a51d3a395a9dae8c3a8a29
farjiiwow <omdewang31122003@gmail.com>
2025-02-03 14:33:24 +0530

    swap using reference

--- C++/YT/swap.cpp
+++ C++/YT/swap.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void swap(int &a, int &b) ;
+
 int main(void) {
     int a;
     int b;
-    int c;
     cout << "Enter two number" << endl;
     scanf("%d", &a);
     scanf("%d", &b);
     cout << "Numbers before swapping" << endl;
     printf("First number : %d\n", a);
     printf("Second number : %d\n", b);
-    c = a;
-    a = b;
-    b = c;
     cout << "Number after swapping are : " << endl;
+    swap(a, b);
     printf("First number : %d\n", a);
     printf("Second number : %d\n", b);
     return 0;
+}
+
+void swap(int &a, int &b) {
+    int swap;
+    swap = a;
+    a = b;
+    b = swap;
 }
