d6d896324ef301303dffb51ca45476229f34eb8d
farjiiwow <omdewang31122003@gmail.com>
2025-02-03 19:38:19 +0530

    move semantics

---
+++ C++/YT/moveSemantics.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) ;
+
+int main(void) {
+    int a;
+    int b;
+    cout << "Enter two number" << endl;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    cout << "Numbers before swapping" << endl;
+    printf("First number : %d\n", a);
+    printf("Second number : %d\n", b);
+    cout << "Number after swapping are : " << endl;
+    swap(a, b);
+    printf("First number : %d\n", a);
+    printf("Second number : %d\n", b);
+    return 0;
+}
+
+void swap(int &a, int &b) {
+    int swap = move(a);
+    a = b;
+    b = swap;
+}
