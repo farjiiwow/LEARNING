4cbe054cc69cb7cb790f826760d11b28a973f5eb
farjiiwow <omdewang31122003@gmail.com>
2025-02-04 20:11:27 +0530

    read a file

--- C++/YT/filesWriting.cpp
+++ C++/YT/filesWriting.cpp
@@ -9,13 +9,22 @@ int main(void) {
     const char * filename = "file.txt";
     const char * information = "lorem ipsum dolor sit amet";
 
-    FILE *fh = fopen(filename, "a");
+    // FILE *fh = fopen(filename, "a");
 
-    for(int i = 0; i < 50; ++i) {
-        fputs(information, fh);
+    // for(int i = 0; i < 50; ++i) {
+    //     fputs(information, fh);
+    // }
+
+    // fclose(fh);
+    
+    char buf[maxbuffer];
+
+    FILE *fh = fopen(filename, "r");
+    while(fgets(buf, maxbuffer, fh)) {
+        fputs(buf, stdout);
     }
 
     fclose(fh);
-    
+
     return 0;
 }
