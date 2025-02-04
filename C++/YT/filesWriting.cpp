b0214cf78c07e427cb9219121eb742ef5aaf6a21
farjiiwow <omdewang31122003@gmail.com>
2025-02-04 20:06:01 +0530

    write mode

---
+++ C++/YT/filesWriting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+constexpr int maxbuffer = 1024;
+
+int main(void) {
+    
+    const char * filename = "file.txt";
+    const char * information = "lorem ipsum dolor sit amet";
+
+    FILE *fh = fopen(filename, "w");
+
+    for(int i = 0; i < 50; ++i) {
+        fputs(information, fh);
+    }
+
+    fclose(fh);
+    
+    return 0;
+}
