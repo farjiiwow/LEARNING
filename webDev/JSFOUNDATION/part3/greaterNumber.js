3f0007b8ec8d54ed608d6251352df5ecc3cd3459
farjiiwow <omdewang31122003@gmail.com>
2025-02-10 20:15:27 +0530

    find greater number

---
+++ webDev/JSFOUNDATION/part3/greaterNumber.js
@@ -0,0 +1,14 @@
+// checking if number is greater then another number
+
+let num1 = 5;
+let num2 = 8;
+
+console.log("I am regular upper code");
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("num2 is greater than num1");
+}
+
+console.log("I am regular bottom code");
