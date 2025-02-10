c5a7b960fb9b8b9da4b768a9dd6c843168875476
farjiiwow <omdewang31122003@gmail.com>
2025-02-10 20:28:04 +0530

    check equal string

---
+++ webDev/JSFOUNDATION/part3/equalString.js
@@ -0,0 +1,10 @@
+// checking if a string is equal to another string
+
+let username = "chai";
+let anotherUsername = "chai";
+
+if (username == anotherUsername) {
+  console.log("Pick another username");
+} else {
+  console.log("You can pick the usename");
+}
