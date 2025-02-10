47307db60272e17dc49acb121cc78746bb49ce2e
farjiiwow <omdewang31122003@gmail.com>
2025-02-10 21:14:59 +0530

    check variable is number

---
+++ webDev/JSFOUNDATION/part3/variableNumber.js
@@ -0,0 +1,9 @@
+// checking if a variable is number or not
+
+let score = 44;
+
+if (typeof score === "number") {
+  console.log("Yep, these is a number");
+} else {
+  console.log("No that is not a number");
+}
