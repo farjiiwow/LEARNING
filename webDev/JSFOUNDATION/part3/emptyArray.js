feeb535f9fdb8b4c868944cf2bb0ed873f4ca4d2
farjiiwow <omdewang31122003@gmail.com>
2025-02-10 21:20:14 +0530

    check empty array

---
+++ webDev/JSFOUNDATION/part3/emptyArray.js
@@ -0,0 +1,10 @@
+// check if array is empty or not
+
+let items = [];
+console.log(items.length);
+
+if (items.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is NOT empty");
+}
