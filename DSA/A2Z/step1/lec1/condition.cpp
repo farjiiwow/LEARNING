d6e2b3ab7a92f1dede9a60403242329c257e330a
farjiiwow <omdewang31122003@gmail.com>
2025-02-11 17:57:33 +0530

    condition statements

---
+++ DSA/A2Z/step1/lec1/condition.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int age = 10;
+
+    if (age >= 18)
+    {
+        cout << "You are an adult." << endl;
+    }
+    else
+    {
+        cout << "You are not an adult." << endl;
+    }
+}
