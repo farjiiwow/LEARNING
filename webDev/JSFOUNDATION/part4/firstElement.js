f81f56c80937d94832937686b3cc299a6e2a7fa0
farjiiwow <omdewang31122003@gmail.com>
2025-02-12 16:17:55 +0530

    access first element

---
+++ webDev/JSFOUNDATION/part4/firstElement.js
@@ -0,0 +1,7 @@
+let teaFlavors = ["green tea", "black tea", "oolong tea"];
+
+// let teaFl = new Array("green tea", "black tea", "oolong tea");
+
+const firstTea = teaFlavors[0];
+
+console.log(firstTea);
