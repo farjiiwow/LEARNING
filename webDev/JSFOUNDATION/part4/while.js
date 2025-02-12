1bc6dfa6999c6584f8863850788229b5b0eb2e0b
farjiiwow <omdewang31122003@gmail.com>
2025-02-12 19:08:22 +0530

    while loop

---
+++ webDev/JSFOUNDATION/part4/while.js
@@ -0,0 +1,30 @@
+let sum = 0;
+
+let i = 1;
+
+while (i < 6) {
+  sum = sum + i;
+  i++;
+}
+
+console.log(sum);
+
+let countdown = [];
+
+// let j = 5;
+// let k = 0;
+// while (j != 0) {
+//   countdown[k] = j;
+//   j--;
+//   k++;
+// }
+
+// console.log(countdown);
+
+let j = 5;
+while (j > 0) {
+  countdown.push(j);
+  j--;
+}
+
+console.log(countdown);
