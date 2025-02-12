abb62d684e566f450952caf820aa4633d562b033
farjiiwow <omdewang31122003@gmail.com>
2025-02-12 19:43:04 +0530

    for loop

---
+++ webDev/JSFOUNDATION/part4/for.js
@@ -0,0 +1,17 @@
+let number = [2, 4, 6];
+let multipliedNumbers = [];
+
+for (let i = 0; i < number.length; i++) {
+  multipliedNumbers.push(number[i] * 2);
+}
+
+console.log(multipliedNumbers);
+
+let cities = ["paris", "new york", "tokyo", "london"];
+let cityList = [];
+
+for (let i = 0; i < cities.length; i++) {
+  cityList.push(cities[i]);
+}
+
+console.log(cityList);
