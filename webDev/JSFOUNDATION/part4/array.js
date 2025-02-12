4feb63ce07419f6c9070bb7e1ffe49915383ecec
farjiiwow <omdewang31122003@gmail.com>
2025-02-12 16:22:55 +0530

    access third element

--- webDev/JSFOUNDATION/part4/array.js
+++ webDev/JSFOUNDATION/part4/array.js
@@ -5,3 +5,9 @@ let teaFlavors = ["green tea", "black tea", "oolong tea"];
 const firstTea = teaFlavors[0];
 
 console.log(firstTea);
+
+let cities = ["London", "Tokyo", "Paris", "New York"];
+
+let favoriteCity = cities[2];
+
+console.log(favoriteCity);
