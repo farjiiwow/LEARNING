a4d61455a422a587498871c8991d65a8325bc4d6
farjiiwow <omdewang31122003@gmail.com>
2025-02-13 16:57:58 +0530

    for loop

---
+++ DSA/A2Z/step1/lec1/forLoop.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    for (int i = 0; i < 11; i++)
+    {
+        cout << "Hey farjii it's " << i << " th iteration" << endl;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "i = " << i << " j = " << j << endl;
+        }
+    }
+
+    return 0;
+}
