99e823f1026a709a3008205652b008e8ea2bf195
farjiiwow <omdewang31122003@gmail.com>
2025-02-14 22:38:36 +0530

    implicit arrow function

--- webDev/JSFOUNDATION/part5/arrowFunction.js
+++ webDev/JSFOUNDATION/part5/arrowFunction.js
@@ -1,6 +1,4 @@
-let calculateTotal = (price, quantity) => {
-  return price * quantity;
-};
+let calculateTotal = (price, quantity) => price * quantity; // implicit arrow function
 
 let totalCost = calculateTotal(499, 100);
 console.log(totalCost);
