f51cbcca9412ef029529d95c8578721f9503006f
farjiiwow <omdewang31122003@gmail.com>
2025-02-17 18:08:42 +0530

    understand scope in closure

--- webDev/JSFOUNDATION/part5/closures.js
+++ webDev/JSFOUNDATION/part5/closures.js
@@ -1,9 +1,10 @@
-function createTeaMaker() {
+function createTeaMaker(name) {
+  let score = 100;
   return function (teaType) {
-    return `Making ${teaType}`;
+    return `Making ${teaType} ${name} ${score}`;
   };
 }
 
-let teaMaker = createTeaMaker();
+let teaMaker = createTeaMaker("farjii");
 let result = teaMaker("green tea");
 console.log(result);
