18f15bfa1437621b1447008535c0ef69649c8613
farjiiwow <omdewang31122003@gmail.com>
2025-02-19 23:09:56 +0530

    explore dunder prototype

--- webDev/JSFOUNDATION/part6/prototypes.js
+++ webDev/JSFOUNDATION/part6/prototypes.js
@@ -1,13 +1,22 @@
-let computer = { cpu: 12 };
+let computer = { cpu: 12, ram: 24 };
 let lenovo = {
   screen: "HD",
+  "refresh rate": 144,
   __proto__: computer,
 };
-let tomHardware = {};
+let tomHardware = {
+  // __proto__: computer,
+  __proto__: lenovo,
+};
 
+console.log(`computer `, computer);
 console.log(`computer `, computer.__proto__);
 console.log(`lenovo `, lenovo.__proto__);
+console.log(`lenovo `, lenovo.ram);
 console.log(`lenovo `, lenovo);
+console.log(`lenovo `, lenovo["refresh rate"]);
+console.log(`tomHardware `, tomHardware.__proto__);
+console.log(`tomHardware `, tomHardware.cpu);
 
 let genericCar = { tyres: 4 };
 let tesla = {
@@ -15,5 +24,7 @@ let tesla = {
 };
 
 Object.setPrototypeOf(tesla, genericCar);
-// console.log(`tesla `, tesla);
+console.log(`tesla `, tesla);
+console.log(`tesla `, tesla.tyres);
+console.log(`tesla `, tesla.driver);
 console.log(`tesla `, Object.getPrototypeOf(tesla));
