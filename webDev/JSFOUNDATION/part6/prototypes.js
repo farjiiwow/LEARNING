fc04d1986a2da173d29bdde6e73bcc57da0c03c4
farjiiwow <omdewang31122003@gmail.com>
2025-02-19 22:53:56 +0530

    dunder prototype

---
+++ webDev/JSFOUNDATION/part6/prototypes.js
@@ -0,0 +1,19 @@
+let computer = { cpu: 12 };
+let lenovo = {
+  screen: "HD",
+  __proto__: computer,
+};
+let tomHardware = {};
+
+console.log(`computer `, computer.__proto__);
+console.log(`lenovo `, lenovo.__proto__);
+console.log(`lenovo `, lenovo);
+
+let genericCar = { tyres: 4 };
+let tesla = {
+  driver: "AI",
+};
+
+Object.setPrototypeOf(tesla, genericCar);
+// console.log(`tesla `, tesla);
+console.log(`tesla `, Object.getPrototypeOf(tesla));
