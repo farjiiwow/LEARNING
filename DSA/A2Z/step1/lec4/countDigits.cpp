9b5be9e8f4cbd902bb103d4cc280aaf3f5b625b3
farjiiwow <omdewang31122003@gmail.com>
2025-03-04 20:49:42 +0530

    count digit optimize

--- DSA/A2Z/step1/lec4/countDigits.cpp
+++ DSA/A2Z/step1/lec4/countDigits.cpp
@@ -9,12 +9,7 @@ public:
 
 int Count::countDigits(int &n)
 {
-    int count = 0;
-    while (n != 0)
-    {
-        count = count + 1;
-        n = n / 10;
-    }
+    int count = (int)(log10(n) + 1);
     return count;
 }
 
