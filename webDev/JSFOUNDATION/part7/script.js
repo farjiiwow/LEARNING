21f6c8bde0941e8e96b2f81ba8204fff5609c72a
farjiiwow <omdewang31122003@gmail.com>
2025-04-08 18:25:13 +0530

    intro to DOM & BOM

---
+++ webDev/JSFOUNDATION/part7/script.js
@@ -0,0 +1,7 @@
+console.log("hello from javascript");
+console.log(window);
+console.log(navigator);
+console.log(location);
+console.log(document.firstChild);
+console.log(document.firstChild.firstChild);
+console.log(document.getElementsByTagName("h1"));
