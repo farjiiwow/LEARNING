4a55ae7bf161af9e0569da2d855df65a497e3179
farjiiwow <omdewang31122003@gmail.com>
2025-04-08 16:47:43 +0530

    operator

---
+++ javascript/Docs/fundamentals/operators/operators.js
@@ -0,0 +1,51 @@
+// unary
+let x = 1;
+console.log("Unary");
+console.log(-x);
+
+// binary
+let y = 3;
+console.log("binary");
+console.log(y - x);
+
+// reminder %
+console.log("reminder");
+console.log(5 % 2);
+console.log(8 % 3);
+console.log(8 % 4);
+
+// exponential
+console.log("exponent");
+console.log(2 ** 2);
+console.log(2 ** 3);
+console.log(2 ** 4);
+
+// root
+console.log("root");
+console.log(2 ** (1 / 2));
+console.log(4 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+// string concatenation
+console.log("string concatenation");
+let s = "my" + "string";
+console.log(s);
+console.log("1" + 1);
+console.log(1 + "1");
+console.log();
+console.log(2 + 2 + "1");
+console.log();
+console.log("1" + 2 + 2);
+
+// only + operator do concatenation rest other operator perform mathematical operation
+
+console.log(6 - "2");
+console.log("6" / "2");
+
+// unary +
+// is is like a Number()
+console.log("uanry +");
+let apple = "2";
+let orange = "3";
+console.log(apple + orange);
+console.log(+apple + +orange);
