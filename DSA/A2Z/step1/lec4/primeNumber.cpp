d9941f944ad2812831c4597762cfe9e82566521e
farjiiwow <omdewang31122003@gmail.com>
2025-04-27 19:21:14 +0530

    optimal approach for prime number

--- DSA/A2Z/step1/lec4/primeNumber.cpp
+++ DSA/A2Z/step1/lec4/primeNumber.cpp
@@ -8,21 +8,29 @@ using namespace std;
 class PrimeNumber
 {
 public:
+    static int count;
     int num;
     bool primeNumber(int);
 };
 
+int PrimeNumber::count = 0;
+
 bool PrimeNumber::primeNumber(int num)
 {
-    if (num >= 2)
+    for (int i = 1; i <= sqrt(num); i++)
     {
-        for (int i = 2; i < num; i++)
+        if (num % i == 0)
         {
-            if (num % i == 0)
+            count = count + 1;
+
+            if (i != num % i)
             {
-                return false;
+                count = count + 1;
             }
         }
+    }
+    if (count == 2)
+    {
         return true;
     }
     else
