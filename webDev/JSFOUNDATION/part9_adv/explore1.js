8bb0f3ce97a08990757ad77f67c405761b46d97c
farjiiwow <omdewang31122003@gmail.com>
2025-05-03 15:48:34 +0530

    explore asynchronous nature

---
+++ webDev/JSFOUNDATION/part9_adv/explore1.js
@@ -0,0 +1,13 @@
+console.log("farjii wow");
+
+function sayHello() {
+  console.log("hello my friend");
+}
+
+setTimeout(() => {
+  sayHello();
+}, 4000);
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
