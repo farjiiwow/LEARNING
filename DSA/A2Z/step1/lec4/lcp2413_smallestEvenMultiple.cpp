8d4099d7c8dd58b7188acb38545f423e5a5ac6fb
farjiiwow <omdewang31122003@gmail.com>
2025-05-10 15:12:03 +0530

    smallest even multiple

---
+++ DSA/A2Z/step1/lec4/lcp2413_smallestEvenMultiple.cpp
@@ -0,0 +1,18 @@
+// TC -> O(1)
+// SC -> O(1)
+
+class Solution
+{
+public:
+    int smallestEvenMultiple(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return n;
+        }
+        else
+        {
+            return 2 * n;
+        }
+    }
+};
